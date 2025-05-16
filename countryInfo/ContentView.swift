76a421d4d3df46e2daf7a3e735c1ce1d3bcbc349
leoli7405@gmail.com <leoli7405@gmail.com>
2025-05-15 19:01:31 -0700

    Initial Commit

---
+++ countryInfo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  countryInfo
+//
+//  Created by Leo Li on 5/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ countryInfo/countryInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  countryInfoApp.swift
+//  countryInfo
+//
+//  Created by Leo Li on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct countryInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
